1f10235738f5ea5685b4859178afa8a30e98cd84
Raga Bharani <ragacb@gmail.com>
2014-06-16 11:05:05 +0530

    a
    a

--- define.js
+++ define.js
@@ -18,6 +18,7 @@ function check()
 	else
 	{
 		document.getElementById('appear').innerHTML='Sorry..Try again';
+		document.getElementById('number').value=0;
 	}
 }
 
@@ -32,26 +33,69 @@ function decrease()
 }
 
 
+function easy0()
+{
+	list.item(0).style.visibility="hidden";
+	decrease();
 	
+}
+function easy1()
+{
+	$("#clue").click(function(){
+	list.item(1).style.visibility="hidden";
+	decrease();
+	});
+}
+function easy2()
+{
+	$("#clue").click(function(){
+	list.item(2).style.visibility="hidden";
+	decrease();
+	});
+}
+function easy3()
+{
+	$("#clue").click(function(){
+	list.item(3).style.visibility="hidden";
+	decrease();
+	});
+}
+function easy4()
+{
+	$("#clue").click(function(){
+	list.item(4).style.visibility="hidden";
+	decrease();
+	});
+}
+function easy5()
+{
+	$("#clue").click(function(){
+	list.item(5).style.visibility="hidden";
+	decrease();
+	});
+}
+
+
 function easy()
 {
-	/*for(i=0;i<l;i++)
-	{
-		$("#clue").click(function(){
-			list.item(l-i).style.visibility="hidden";
-		});
-		
-	}
-	*/
-	$("#clue").click(function(){
+
+	/*$("#clue").click(function(){
 			list.item(i).style.visibility="hidden";
 			decrease();
-		
-		if(i<l)
-		{easy();}
-		});
-		i++;
+			if(i<l)
+		{   
+			easy();
+		}
+		});*/
+		if(document.getElementById('clue').clicked==true)
+		{easy0();}
 			
+		easy1();
+		easy2();	
+		easy3();
+		easy4();	
+		easy5();
+		
 }
 
 $("#easy").click(function(){
